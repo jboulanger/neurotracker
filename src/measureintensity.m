c8774a8c3b0043ebe0e1e5551b6d8a99ae8ee465
Jerome Boulanger <jeromeb@mrc-lmb.cam.ac.uk>
2017-04-26 10:56:19 +0100

    "clean up and change fractions"

--- src/measureintensity.m
+++ src/measureintensity.m
@@ -1,15 +1,15 @@
-function [region1,region2] = measure_intensity(img,x,y,r1,r2,pvalue,smooth)
-% [region1,region2] = measure_intensity(img,x,y,r1,r2,pvalue)
+function [region1,region2] = measure_intensity(img,x,y,r1,r2,p1,p2,smooth)
+% [region1,region2] = measure_intensity(img,x,y,r1,r2,pvalue,smooth)
 %
 % Measure the intensities in two disks of radius r1 & r2 around position (x,y)
 % Only a fraction of the pixel intensity is taken into account (pvalue)
 % 
 % In the 1st ring r<r1, the average of 
-%        intensity > cdfinv(pvalue) 
+%        intensity > cdfinv(p1) 
 % is measured.
 %
 % In the 2nd ring r>r1 & r<r2, the average of
-%    cdfinv(pvalue/2) < intensity < cdfinv(1-pvalue/2) 
+%    cdfinv(p2/2) < intensity < cdfinv(1-p2/2) 
 % is measured.
 % 
 % Jerome Boulanger 2017
@@ -21,9 +21,10 @@ S = imfilter(double(img), filter, 'symmetric');
 % distance function centered on (x,y)
 [X,Y] = meshgrid(1:size(img,2),1:size(img,1));
 d = sqrt((X-x).^2+(Y-y).^2);
+
 % measure mean intensity for r < r1 & I > alpha
-region1 = segment_bright_blob(S, d < r1, img, pvalue);
-region2 = segment_background(S, d > r1 & d < r2, img, 0.1);
+region1 = segment_bright_blob(S, d < r1, img, p1);
+region2 = segment_background(S, d > r1 & d < r2, img, p2);
 
 end
 
